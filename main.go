f1f9ff9d15177c5c52dac6f1e04bb260e308b498
Ade Dwi Putra <ade@ainosi.co.id>
2023-12-13 14:02:16 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	if err := app.Listen(":8080"); err != nil {
+		panic(err)
+	}
+}
